df4ac4ed15e7937f49568bcb9dbf37e52d0f0a7d
Akkiiimehta <101335442+Akkiiimehta@users.noreply.github.com>
2026-08-19 10:13:15 +0530

    Update InfoOverlay.tsx

--- components/InfoOverlay.tsx
+++ components/InfoOverlay.tsx
@@ -36,6 +36,7 @@ export default function InfoOverlay({ onClose }: InfoOverlayProps) {
   <a className="contact-email" href="mailto:yashmehtaoffice@gmail.com">
     yashmehtaoffice@gmail.com
   </a>
+              <span> · </span>
   <a className="contact-phone" href="tel:+917021291405">
     +91 70212 91405
   </a>
